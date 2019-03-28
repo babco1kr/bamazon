fc1ba01bf2dcd43aba0f8ba76f8992e698a0ba18
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-28 19:53:32 -0400

    table made with database info for termial

--- Javascript/bamazon.js
+++ Javascript/bamazon.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -20,8 +21,38 @@ var connection = mysql.createConnection({
       connection.query("select * from products", function(error, results){
           if (error) throw error;
 
+          var table = new Table({
+              head: ["ID", "Name", "Department", "Price", "In Stock"],
+              colWidths: [10, 20, 20, 20, 10]
+          });
+
           for (i = 0; i < results.length; i ++) {
-            console.log(results[i]);
+              table.push(
+                  [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
+              );
           }
+          console.log(table.toString());
+          buy();
       });
+  }
+
+  function buy () {
+      inquirer.prompt([
+          {
+              type: "input",
+              message: "What is the ID of the product you'd like to buy?",
+              name: "id"
+          },
+          {
+              type: "input",
+              message: "How much would you like to buy?",
+              name: "amount"
+          }
+      ]).then(function(answer) {
+          connection.query(
+              "select `stock_quantity` where `item_id` = ?", answer.id
+          )
+
+          console.log(answer.id);
+      })
   }
